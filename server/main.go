67fa6ca61afa7af01f5d8f4d5deaf1ece04d5522
pogtato <69479556+narutopig@users.noreply.github.com>
2022-12-01 08:15:16 -0800

    changed ws api route

--- server/main.go
+++ server/main.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"time"
 )
 
 var upgradeHandler = websocket.Upgrader{}
 
-//default upgrade settings
+// default upgrade settings
 
-//doing the funny to make a func for a simple error check
-
+// doing the funny to make a func for a simple error check
 func checkErr(err error) {
 	if err != nil {
 		log.Println("ERROR THROWN: ")
@@ -25,22 +25,19 @@ func checkErr(err error) {
 }
 
 func sendPacket(co *websocket.Conn) {
-	//even tho cotnent is techncially a byte array its lkay of io use any
-	//experimental forever loop
+	// even tho cotnent is techncially a byte array its lkay of io use any
+	// experimental forever loop
 
 	msgT, cont, err := co.ReadMessage()
-	//assuming that error is not existent
-	checkErr(err) //will break out with panic log if error
+	// assuming that error is not existent
+	checkErr(err) // will break out with panic log if error
 	for {
 		time.Sleep(time.Second)
-		co.WriteMessage(msgT, cont);
+		co.WriteMessage(msgT, cont)
 	}
-
-	
-
 }
 
-func connectHandler(w http.ResponseWriter, r *http.Request) { //value not reference
+func connectHandler(w http.ResponseWriter, r *http.Request) { // value not reference
 
 	connection, err := upgradeHandler.Upgrade(w, r, nil)
 	checkErr(err)
@@ -49,7 +46,6 @@ func connectHandler(w http.ResponseWriter, r *http.Request) { //value not refere
 	log.Println("Connection Established")
 
 	connectReader(connection)
-
 }
 
 func connectReader(co *websocket.Conn) {
@@ -60,7 +56,7 @@ func connectReader(co *websocket.Conn) {
 		fmt.Println("Read message: " + string(content))
 
 		if err := co.WriteMessage(messageType, content); err != nil {
-			//retursne rror if comes across one, will execute writemssage func and will nly retirn error if there is one
+			// retursne rror if comes across one, will execute writemssage func and will nly retirn error if there is one
 			log.Panic(err)
 		}
 
@@ -76,8 +72,8 @@ func main() {
 
 	api := router.Group("/api")
 	{
-		api.GET("/", func(c *gin.Context) {
-			connectHandler(c.Writer, c.Request) //on req establish connection or just in general
+		api.GET("/ws", func(c *gin.Context) {
+			connectHandler(c.Writer, c.Request) // on req establish connection or just in general
 		})
 	}
 
